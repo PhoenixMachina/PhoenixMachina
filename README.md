bfcf23bb1d7e15a392e9d781503be320b174d923
Nathanaël Langlois <nathanael.langlois@gmail.Com>
2016-01-26 20:18:42 +0100

    Database requirements added

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 # PhoenixMachina
 
-## Backend requirements 
+## Backend requirements
+
+### Packages
 You need to have install the following Julia packages on your computer :
 - HttpServer
 
 To install, open a Julia console, and type Pkg.add("HttpServer")
 
+### Database
+You need to create a database phoenixmachina and import the file phoenixmachina.sql which contains the tables you'll need.
+
 ## Settings
 ### Required
 The path variable in config.jl must be set to your path
 
-## Start 
+## Start
 Launch start_server.jl. You should see "Listening to "...
 
 You then just need to open your browser and type localhost:8000/PhoenixMachina
---
+++ phoenixmachina.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 26, 2016 at 08:14 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `phoenixmachina`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(80) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `avatar` varchar(50) NOT NULL,
+  `register_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `lastlogin_date` timestamp NULL DEFAULT NULL,
+  `group` int(11) NOT NULL,
+  `ip` varchar(50) NOT NULL,
+  `account_validated` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
