5e2f71bf415fb8bbc3d2a11c0c9c25c6c4a0a98f
Nathanael Langlois <nathanael.langlois@gmail.com>
2016-01-29 11:36:59 +0100

    Parser corrected

--- include/ResponseHandler.jl
+++ include/ResponseHandler.jl
@@ -2,7 +2,7 @@ function parseView(response)
   for match in eachmatch(r"\$\{([a-zA-Z0-9_]+)\}",response)
     if isdefined(symbol((match.match)[3:end-1]))
       var = @eval ($(symbol((match.match)[3:end-1])))
-      response = string(response[1:(match.offset)],var,response[((match.offset)+(length(match.match))):end] )
+      response = string(response[1:(match.offset)-1],var,response[((match.offset)+(length(match.match))):end] )
     end
   end
 
