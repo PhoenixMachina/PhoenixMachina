4a42d5849fb621d604f56fe26fe9d125f9e073d6
Nathanaël Langlois <nathanael.langlois@gmail.Com>
2016-01-29 11:11:53 +0100

    Parser added

--- include/ResponseHandler.jl
+++ include/ResponseHandler.jl
@@ -1,3 +1,9 @@
 function parseView(response)
+  for match in eachmatch(r"\$\{([a-zA-Z0-9_]+)\}",response)
+    if isdefined(symbol((match.match)[3:end-1]))
+      response = string(response[1:(match.offset)]),@eval ($(symbol((match.match)[3:end-1]))),response[((match.offset)+(length(match.match))):end] )
+    end
+  end
+
   return response
 end
