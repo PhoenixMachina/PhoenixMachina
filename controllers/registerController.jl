edd3fd77e3400204489fc8aa97a6808ff100320a
Mexanga <mexanga@gmail.com>
2016-02-08 21:51:15 +0100

    Adding Post Variables for register

--- controllers/registerController.jl
+++ controllers/registerController.jl
@@ -4,5 +4,21 @@ function getContent(req::Request, res::Response)
 end
 
 function postContent(req::Request, res::Response)
-  Response("You might be able to register here one day")
+  registerPage = Page("main.html","register.html",Dict())
+  # Set post variables
+  global pseudo = ""
+  global password = ""
+  global mail = ""
+  # Check all values of post requests
+  for n in eachindex(foo)
+    global pseudo = foo[n][1] == "pseudo" ? foo[n][2] : !is_null(pseudo) ? pseudo : ""
+    global password = foo[n][1] == "password" ? foo[n][2] : !is_null(password) ? password : ""
+    global mail = foo[n][1] == "mail" ? foo[n][2] : !is_null(mail) ? mail : ""
+  end
+  if !is_null(pseudo) && !is_null(password) && !is_null(mail)
+    # etc...
+   else
+    critical("Have you defined all inputs ?")
+   end
+  Response(getParsedContent(registerPage))
 end
