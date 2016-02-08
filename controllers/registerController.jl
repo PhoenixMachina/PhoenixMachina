2ce7fba68c2f8f8f6229010920e3469e38315a53
mexanga <mexanga@gmail.com>
2016-02-08 19:17:37 +0100

    Fix last commit of registerController.jl

--- controllers/registerController.jl
+++ controllers/registerController.jl
@@ -4,16 +4,15 @@ function getContent(req::Request, res::Response)
 end
 
 function postContent(req::Request, res::Response)
+  # Check all values of post requests
   for n in eachindex(foo)
-    global pseudo = foo[n][1] == "pseudo" ? foo[n][2] : isdefined(:pseudo) ? pseudo : ""
-    global password = foo[n][1] == "password" ? foo[n][2] : isdefined(:password) ? password : ""
-    global mail = foo[n][1] == "mail" ? foo[n][2] : isdefined(:mail) ? mail : ""
+    global pseudo = foo[n][1] == "pseudo" ? foo[n][2] : !is_null(pseudo) ? pseudo : ""
+    global password = foo[n][1] == "password" ? foo[n][2] : !is_null(password) ? password : ""
+    global mail = foo[n][1] == "mail" ? foo[n][2] : !is_null(mail) ? mail : ""
   end
   registerPage = Page("main.html","register.html",Dict())
   if !is_null(pseudo) && !is_null(password) && !is_null(mail)
-    println("pseudo: ", pseudo)
-    println("password: ", password)
-    println("mail: ", mail)
+    # etc...
    else
     critical("Have you defined all inputs ?")
    end
