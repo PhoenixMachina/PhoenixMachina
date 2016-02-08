7b686db6efa915d937f2d970b2cbffa47a443fc4
Mexanga <mexanga@gmail.com>
2016-02-08 23:02:25 +0100

    Update functions.jl

--- include/functions.jl
+++ include/functions.jl
@@ -1,5 +1,5 @@
 # is_null() checks if the variable chosen is empty
-function is_null(var::Any)
-  var = replace(var, " ", "") # Delete space
-  return var == "" ? true : false
+function is_null(arg::Any)
+  arg = replace(arg, " ", "") # Delete space
+  return arg == "" || arg == 0 ? true : false
 end
