abe85db9a6905819ccd3898479ffaed9cf95e502
Nathanael Langlois <nathanael.langlois@gmail.com>
2016-02-14 13:51:53 +0100

    Added toggle

--- resources/js/course.js
+++ resources/js/course.js
@@ -1,3 +1,9 @@
 $('#toggleButton').on('click', function() {
   $('#menuHide').toggle();
+  if(document.getElementById('toggleButton').innerHTML == ">"){
+    document.getElementById('toggleButton').innerHTML = "<";
+  }
+  else {
+    document.getElementById('toggleButton').innerHTML = ">";
+  }
 });
