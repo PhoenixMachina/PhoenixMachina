5030e749d550211b5ebf1b89eac12571fd867e9b
Mexanga <mexanga@gmail.com>
2016-03-19 22:10:28 +0100

    Check if packages are installed

--- install.jl
+++ install.jl
@@ -2,6 +2,10 @@ Pkg.update()
 Pkg.add("HttpServer")
 Pkg.add("Logging")
 Pkg.add("Markdown")
-Pkg.clone("https://github.com/JuliaComputing/MySQL.jl")
-Pkg.clone("https://github.com/PhoenixMachina/Tlaloc")
+if !("MySQL" in keys(Pkg.installed()))
+  Pkg.clone("https://github.com/JuliaComputing/MySQL.jl")
+end
+if !("Tlaloc" in keys(Pkg.installed()))
+  Pkg.clone("https://github.com/PhoenixMachina/Tlaloc")
+end
 Pkg.add("SecureSessions")
