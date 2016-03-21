5c98a5f8329bf7ab6f9e743648c2e540db8b40aa
Nathanael Langlois <nathanael.langlois@gmail.com>
2016-03-21 11:35:03 +0100

    Added courses and private messages tables

--- phoenixmachina.sql
+++ phoenixmachina.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 26, 2016 at 08:14 PM
+-- Generation Time: Mar 21, 2016 at 11:33 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -19,6 +19,164 @@ SET time_zone = "+00:00";
 --
 -- Database: `phoenixmachina`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE IF NOT EXISTS `courses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `logo` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `tags` varchar(255) NOT NULL,
+  `level` varchar(30) NOT NULL,
+  `introduction` text NOT NULL,
+  `conclusion` text NOT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses_chapters`
+--
+
+CREATE TABLE IF NOT EXISTS `courses_chapters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `part_id` int(11) NOT NULL,
+  `order` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `current_commit` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses_commits`
+--
+
+CREATE TABLE IF NOT EXISTS `courses_commits` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author_id` int(11) NOT NULL,
+  `commit_version` varchar(255) NOT NULL,
+  `commit_content` text NOT NULL,
+  `commit_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `chapter_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses_parts`
+--
+
+CREATE TABLE IF NOT EXISTS `courses_parts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `course_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `order` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exercises`
+--
+
+CREATE TABLE IF NOT EXISTS `exercises` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(11) NOT NULL,
+  `question` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exercises_code`
+--
+
+CREATE TABLE IF NOT EXISTS `exercises_code` (
+  `id_exercise` int(11) NOT NULL,
+  `input` text NOT NULL,
+  `desired_output` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exercises_mcq`
+--
+
+CREATE TABLE IF NOT EXISTS `exercises_mcq` (
+  `id_exercise` int(11) NOT NULL,
+  `answer` text NOT NULL,
+  `correct` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exercises_user_code`
+--
+
+CREATE TABLE IF NOT EXISTS `exercises_user_code` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_exercise` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `language` varchar(50) NOT NULL,
+  `code` text NOT NULL,
+  `completed` int(11) NOT NULL,
+  `error_msg` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pm`
+--
+
+CREATE TABLE IF NOT EXISTS `pm` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `subtitle` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `nb_answers` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='private messages' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pm_messages`
+--
+
+CREATE TABLE IF NOT EXISTS `pm_messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `message` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pm_users`
+--
+
+CREATE TABLE IF NOT EXISTS `pm_users` (
+  `pm_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
