65541226a95f83be30003a180bc1ba92ee04304f
Nathanael Langlois <nathanael.langlois@gmail.com>
2016-04-09 14:22:10 +0200

    Added tag and untag functions

--- modules/CourseEditorModule.jl
+++ modules/CourseEditorModule.jl
@@ -1,11 +1,25 @@
 module CourseEditorModule
+  export tagCourse,unTagCourse
+
   using CourseModule
 
-  function tag(course::Course,tag::ASCIIString)
-    #TODO
+  # Tags a course
+  function tagCourse(course::Course,tag::ASCIIString)
+    course.tags = string(course.tags,",",tag)
   end
-  function unTag(course::Course,tag::ASCIIString)
-    #TODO
+
+  # Untags course
+  function unTagCourse(course::Course,tag::ASCIIString)
+    tagMatch = match(Regex(tag),course.tags)
+    if tagMatch == nothing # if there's no match
+      return false
+    end
+    if tagMatch.offset + length(tag) == length(course.tags) # if it's the last tag
+      course.tags = (course.tags)[1:tagMatch.offset]
+    else
+      course.tags = string((course.tags)[1:tagMatch.offset-1],(course.tags)[tagMatch.offset+length(tag)+1:end])   # otherwhise we'll cut the tag from the rest without forgetting the comma
+    end
+    return true
   end
 
 end
